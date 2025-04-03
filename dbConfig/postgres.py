184e8547c1370a0193a5436755cd8843d67d6f47
franciscoL0pez <franlopez.2001@hotmail.com>
2025-04-02 22:33:55 -0300

    Refactor test

--- dbConfig/postgres.py
+++ dbConfig/postgres.py
@@ -1,26 +1,54 @@
 import psycopg2
 
-def connect_to_db():
-    """Intenta conectarse a PostgreSQL usando valores fijos."""
+import psycopg2
+import os
+
+# This credential need to put in envioroment variable
+DB_USER = "postgres"
+DB_PASSWORD = 1234
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "test_db"
+
+def create_database():
+    """Crea la base de datos si no existe."""
     try:
-    
-        dbname = "test_db"
-        user = "postgres"
-        password = "1234"
-        host = "localhost"
-        port = "5432"
-
-
+       
         conn = psycopg2.connect(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host=host,
-            port=port
+            dbname="postgres",  
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
         )
-        print("✅ Conexión exitosa a PostgreSQL")
-        return conn
+        conn.autocommit = True
+        cur = conn.cursor()
 
+      
+        cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}';")
+        exists = cur.fetchone()
+
+      
+        if not exists:
+            cur.execute(f"CREATE DATABASE {DB_NAME};")
+            print(f"✅ Base de datos '{DB_NAME}' creada.")
+
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"⚠️ No se pudo crear la base de datos: {e}")
+
+def connect_to_db():
+    """Intenta conectarse a la base de datos."""
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        return conn
     except Exception as e:
         print(f"❌ Error de conexión: {e}")
         return None
--- tests/test_postgres_connect.py
+++ tests/test_postgres_connect.py
@@ -1,5 +1,6 @@
 import pytest
 from dbConfig.postgres import connect_to_db  # Asegúrate de que el import sea correcto
+from dbConfig.postgres import create_database  # Asegúrate de que el import sea correcto
 import sys
 import os
 
@@ -8,6 +9,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 def test_connection_success():
     """Verifica que la conexión a PostgreSQL sea exitosa."""
+
+    create_database()
+ 
     conn = connect_to_db()
     assert conn is not None, "No se pudo conectar a la base de datos"
     if conn:
